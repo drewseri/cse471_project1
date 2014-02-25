ddf90a58c17560c350015bd0cc1653110ddc0e1b
bonnenbe <bonnenbe@msu.edu>
2014-02-25 13:35:56 -0500

    Reupload with Ben's step 5

---
+++ Synthie/SineWave.cpp
@@ -0,0 +1,29 @@
+#include "StdAfx.h"
+#include "SineWave.h"
+
+#include <cmath>
+
+
+CSineWave::CSineWave(void)
+{
+}
+
+
+CSineWave::~CSineWave(void)
+{
+}
+
+void CSineWave::Start()
+{
+    m_phase = 0;
+}
+
+bool CSineWave::Generate()
+{
+    m_frame[0] = m_amp * sin(m_phase * 2 * PI);
+    m_frame[1] = m_frame[0];
+
+    m_phase += m_freq * GetSamplePeriod();
+
+    return true;
+}
