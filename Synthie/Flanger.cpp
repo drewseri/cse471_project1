76d8a9f57b64a62c02165c7118d53c41d8b7fc46
joineral32 <joineral@msu.edu>
2014-03-13 19:41:57 -0400

    Update to flanger
    updated the flanger effect

--- Synthie/Flanger.cpp
+++ Synthie/Flanger.cpp
@@ -15,11 +15,13 @@ void CFlanger::ProcessWet(double * frame){
 	for (int i = 0; i < 2; i++){
 		inQueue[(wrloc+i)%QUEUESIZE] = frame[i];
 	}
-	int delaySize = int(88.2*(25 + 5 * sin(phaseAngle)));
+	int delaySize = int(88.2*(6 + 4 * sin(phaseAngle)));
+	int otherDelaySize = int(88.2*(10 + 8 * cos(phaseAngle)));
 	phaseAngle += 2*M_PI*1/44100;
 	for (int i = 0; i < 2; i++){
-		frame[i] = inQueue[(wrloc+i+delaySize)%QUEUESIZE]*(0.75+0.25*sin(phaseAngle*2));
-		frame[i] /= 2;
+		frame[i] += inQueue[(wrloc+i+delaySize)%QUEUESIZE]*(0.75+0.25*sin(phaseAngle*2));
+		frame[i] += inQueue[(wrloc+i+otherDelaySize)%QUEUESIZE]*(0.75+0.25*sin(2*phaseAngle*2));
+		frame[i] /= 3;
 	}
 	for (int i = 0; i < 2; i++){
 		outQueue[(wrloc+i)/QUEUESIZE] = frame[i];
